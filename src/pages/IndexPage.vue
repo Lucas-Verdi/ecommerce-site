b5f8f1766104ec6851bd7c25528d1388db495291
Lucas Verdi <lucas.a.verdi@gmail.com>
2024-01-12 17:56:23 -0300

    Carrossel de promoções

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -16,29 +16,37 @@
           {{ lorem }}
         </div>
       </q-carousel-slide>
-      <q-carousel-slide name="layers" class="column no-wrap flex-center">
-        <q-icon name="layers" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
+    </q-carousel>
+  </div>
+
+
+  <div class="q-pa-md">
+    <q-carousel class="bg-grey-1" animated swipeable transition-prev="jump-right" transition-next="jump-left" v-model="slide2" arrows infinite control-color="black" height="300px">
+      <q-carousel-slide :name="1" class="column no-wrap flex-center">
+        <div class="row text-center">
+          <div v-for="p in promocoes" :key="p.produto" class="col-3 q-pl-xl">
+            <q-card filled bordered class="my-card">
+              <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <q-card-section>{{ p.produto }}</q-card-section>
+              <q-card-section>{{ p.preco }}</q-card-section>
+            </q-card>
+          </div>
         </div>
       </q-carousel-slide>
-      <q-carousel-slide name="map" class="column no-wrap flex-center">
-        <q-icon name="terrain" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
+
+      <q-carousel-slide :name="2" class="column no-wrap flex-center">
+        <div class="row text-center">
+          <div v-for="p in promocoes2" :key="p.produto" class="col-3 q-pl-xl">
+            <q-card filled bordered class="my-card">
+              <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <q-card-section>{{ p.produto }}</q-card-section>
+              <q-card-section>{{ p.preco }}</q-card-section>
+            </q-card>
+          </div>
         </div>
       </q-carousel-slide>
     </q-carousel>
   </div>
-
-  <div class="row q-pl-md">
-      <div v-for="i in count" :key = i.id class="col-3">
-        <q-card filled bordered class="my-card">
-          Produto teste
-          <img src="https://cdn.quasar.dev/img/parallax2.jpg">
-        </q-card>
-      </div>
-    </div>
 </template>
 
 <script>
@@ -49,9 +57,24 @@ export default {
   setup() {
     return {
       slide: ref('style'),
+      slide2: ref(1),
       count: ref(4),
       autoplay: ref(true),
-      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
+      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.',
+
+      promocoes: [
+        {produto: 'Produto teste', preco: '1'},
+        {produto: 'Produto teste 2', preco: '2'},
+        {produto: 'Produto teste 3', preco: '3'},
+        {produto: 'Produto teste 4', preco: '4'}
+      ],
+
+      promocoes2: [
+        {produto: 'Produto teste 5', preco: '5'},
+        {produto: 'Produto teste 6', preco: '6'},
+        {produto: 'Produto teste 7', preco: '7'},
+        {produto: 'Produto teste 8', preco: '8'}
+      ]
     }
   }
 }
@@ -60,5 +83,5 @@ export default {
 
 <style lang="sass" scoped>
 .my-card
-  width: 95%
+  width: 84%
 </style>
